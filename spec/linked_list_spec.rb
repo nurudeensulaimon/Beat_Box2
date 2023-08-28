b51411860b424b97e4682ee305d446cc4f0f74d0
nurudeensulaimon <nurudeen.a.sulaimon@gmail.com>
2023-08-27 20:25:22 -0400

    Added String and Prepend test 2) Added to_string method 3) Added prepend method

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -15,8 +15,7 @@ RSpec.describe LinkedList do
 
   describe "it has an head" do 
     it "has an head that is nil" do 
-  
-  expect(@list.head).to eq(nil)
+      expect(@list.head).to eq(nil)
   end 
  end
 
@@ -25,7 +24,34 @@ RSpec.describe LinkedList do
       @list.append("doop")
      expect(@list.head.data).to eq("doop")
      expect(@list.head.next_node).to eq(nil)
+     expect(@list.count).to eq(1)
+     expect(@list.to_string).to eq("doop")
    end 
+end 
+
+   describe "it add another data into node" do 
+     it "has a method that append additiona method" do 
+      expect(@list.head).to eq(nil)
+      @list.append("doop")
+      expect(@list.head.data).to eq("doop")
+      expect(@list.head.next_node).to eq(nil)
+
+
+      @list.append("deep")
+      expect(@list.head.next_node.data).to eq("deep")
+      expect(@list.count).to eq(2)
+      expect(@list.to_string).to eq("doop deep")
+     end 
   end 
+
+   describe "has a method prepend" do 
+  it "can add @head to the list" do 
+    @list.append("doop")
+    @list.append("plop")
+    expect(@list.to_string).to eq("doop plop")
+    @list.prepend("dop")
+    expect(@list.to_string).to eq("dop doop plop")
+  end
+  end
 end 
 
